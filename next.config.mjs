1f35f7248c7ab235cfe690db7c640a3c8f5bebcd
Abhishek Raj <abhi@raj.me>
2024-09-15 08:54:46 +0530

    Add support for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  output: process.env.OUTPUT,
+  images: {
+    unoptimized: process.env.OUTPUT === 'export',
+  },
+};
 
 export default nextConfig;
