784272f478239509815acfbc95caaabc47246c4a
Abhishek Raj <abhi@raj.me>
2024-09-17 05:46:15 +0530

    Add zod error messages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,17 +5,34 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Invalid customer',
+    required_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Please enter an amount greater than $0.'
+  }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Invalid invoice status',
+    required_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(_prevState: State, formData: FormData) {
   // const rawFormData = {
   //   customerId: formData.get('customerId'),
   //   amount: formData.get('amount'),
@@ -23,22 +40,27 @@ export async function createInvoice(formData: FormData) {
   // };
   const rawFormData = Object.fromEntries(formData.entries());
 
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: rawFormData.customerId,
+    amount: rawFormData.amount,
+    status: rawFormData.status,
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
   try {
-    const { customerId, amount, status } = CreateInvoice.parse({
-      customerId: rawFormData.customerId,
-      amount: rawFormData.amount,
-      status: rawFormData.status,
-    });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-
-    console.log({
-      customer_id: customerId,
-      amount: amountInCents,
-      status,
-      date,
-    });
-
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
