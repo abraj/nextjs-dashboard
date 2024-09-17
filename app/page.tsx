0cc0495687f37cee09ac79871a7410f07dc8d2a1
Abhishek Raj <abhi@raj.me>
2024-09-17 16:47:51 +0530

    Add auth support

--- app/page.tsx
+++ app/page.tsx
@@ -25,8 +25,8 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            // href="/login"
-            href="/dashboard"
+            href="/login"
+            // href="/dashboard"
             // href="/dashboard/invoices"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
